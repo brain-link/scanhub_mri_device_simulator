41e0b1f61bd6d6334332c39bd84b1aeeba236097
Christoph Dinh <christoph.dinh@mne-cpp.org>
2022-05-09 18:59:11 +0200

    update storage connector

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     print(kspacedata[0][0])
 
     # DEBUG
-    os.environ['STORAGE_CONNECTION_STRING'] = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;'
+    os.environ['STORAGE_CONNECTION_STRING'] = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
     os.environ['STORAGE_CONTAINER'] = 'raw-mri' #str(uuid.uuid4()) #'scanhub-container'
 
     # get storage account settings
