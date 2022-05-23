2517a30f22f1850816692c7118aa4752415d698c
Christoph Dinh <christoph.dinh@mne-cpp.org>
2022-05-24 01:46:36 +0200

    Add commands to acquisition control

--- README.rst
+++ README.rst
@@ -5,6 +5,13 @@ scanhub-mri-device-simulator
 
     python main.py --log
 
+Trigger Measurement via Debug Workflow:
+
+.. code-block:: bash
+
+    http://localhost:81/api/TriggerAcquisition?cmd=MEAS_START
+
+
 development
 ===========
 
--- acquisition_control.py
+++ acquisition_control.py
@@ -5,23 +5,90 @@ from PySide6.QtCore import QObject, QThread, Signal, Slot
 
 import sys
 import random
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+import time, os
+
+from PySide6.QtWidgets import (
+        QApplication,
+        QWidget,
+        QPushButton,
+        QLabel,
+        QVBoxLayout
+)
 from PySide6 import QtGui
 
 from enum import Enum
 
+from azure.storage.queue import (
+        QueueClient,
+        BinaryBase64EncodePolicy,
+        BinaryBase64DecodePolicy
+)
+
+
+#if __name__ == '__main__':
+#    print("AcquisitionControl started")
+
+#    # DEBUG
+#    os.environ['STORAGE_CONNECTION_STRING'] = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
+
+#    # Retrieve the connection string from an environment
+#    # variable named AZURE_STORAGE_CONNECTION_STRING
+#    connect_str = os.getenv("STORAGE_CONNECTION_STRING")
+
+#    # queue name
+#    q_name_tasks = "acquisition-control-queue"
+#    q_name_results = "acquistion-control-results-queue"
+
+#    # connect to queue
+#    queue_client_tasks = QueueClient.from_connection_string(connect_str, q_name_tasks,
+#                            message_encode_policy = BinaryBase64EncodePolicy(),
+#                            message_decode_policy = BinaryBase64DecodePolicy())
+#    queue_client_results = QueueClient.from_connection_string(connect_str, q_name_results,
+#                            message_encode_policy = BinaryBase64EncodePolicy(),
+#                            message_decode_policy = BinaryBase64DecodePolicy())
+#    # queue_client_results.create_queue()
+#    while(True):
+#        messages = queue_client_tasks.receive_messages()
+
+#        for message in messages:
+#            print(message)
+#            print("Dequeueing message: " + message.content.decode('UTF-8'))
+#            queue_client_tasks.delete_message(message.id, message.pop_receipt)
+
+#            print("Adding message: " + message.content.decode('UTF-8'))
+#            queue_client_results.send_message(message.content)
+
+#        time.sleep(1)
+
+
 class AcquisitionCommands(Enum):
-    START = 0
-    STOP = 1
-    PAUSE = 2
+
+    # MEASURMENT
+    MEAS_START = 1000
+    MEAS_STOP = 1001
+    MEAS_PAUSE = 1002
+
 
 class AcquisitionControl(QObject):
 
     signalStatus = Signal(str)
+
     signalStart = Signal()
+
+    signalStartMeasurement = Signal()
+    signalStopMeasurement = Signal()
+    signalPauseMeasurement = Signal()
 
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
+
+        # DEBUG
+        os.environ['STORAGE_CONNECTION_STRING'] = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
+
+        # Retrieve the connection string from an environment
+        # variable named AZURE_STORAGE_CONNECTION_STRING
+        self._connect_str = os.getenv("STORAGE_CONNECTION_STRING")
 
         # Create a new worker thread.
         self.createWorkerThread()
@@ -36,13 +103,14 @@ class AcquisitionControl(QObject):
     def createWorkerThread(self):
 
         # Setup the worker object and the worker_thread.
-        self.worker = WorkerThread()
+        self.worker = WorkerThread(self._connect_str)
         self.worker_thread = QThread()
         self.worker.moveToThread(self.worker_thread)
         self.worker_thread.start()
 
         # Connect any worker signals
         self.worker.signalStatus.connect(self.signalStatus)
+        self.worker.signalCommand.connect(self.processCommand)
         self.signalStart.connect(self.worker.startWork)
 
     @Slot()
@@ -59,41 +127,82 @@ class AcquisitionControl(QObject):
             print('building new working object.')
             self.createWorkerThread()
 
-
     def forceWorkerQuit(self):
         if self.worker_thread.isRunning():
             self.worker_thread.terminate()
             self.worker_thread.wait()
 
+    @Slot()
+    def processCommand(self, acquisitionCommand: AcquisitionCommands) -> bool:
+        print('process Command')
+        match acquisitionCommand:
+            case AcquisitionCommands.MEAS_START:
+                print(AcquisitionCommands.MEAS_START)
+                self.signalStartMeasurement.emit()
+                return True
+            case AcquisitionCommands.MEAS_STOP:
+                print(AcquisitionCommands.MEAS_STOP)
+                self.signalStopMeasurement.emit()
+                return True
+            case AcquisitionCommands.MEAS_PAUSE:
+                print(AcquisitionCommands.MEAS_PAUSE)
+                self.signalPauseMeasurement.emit()
+                return True
+            # default
+            case _:
+                return False
 
 class WorkerThread(QObject):
-    signalStatus = Signal(str)
 
-    def __init__(self, parent=None):
+    signalStatus = Signal(str)
+    signalCommand = Signal(AcquisitionCommands)
+
+    def __init__(self, connect_str: str, parent=None):
         super(self.__class__, self).__init__(parent)
+        self._connect_str = connect_str
 
     @Slot()
-    def startWork(self):
-        for ii in range(7):
-            number = random.randint(0,5000**ii)
-            self.signalStatus.emit('Iteration: {}, Factoring: {}'.format(ii, number))
-            factors = self.primeFactors(number)
-            print('Number: ', number, 'Factors: ', factors)
-        self.signalStatus.emit('Idle.')
+    def startWork(self) -> None:
+        # queue name
+        q_name_tasks = "acquisition-control-queue"
+        q_name_results = "acquistion-control-results-queue"
 
-    def primeFactors(self, n):
-        i = 2
-        factors = []
-        while i * i <= n:
-            if n % i:
-                i += 1
-            else:
-                n //= i
-                factors.append(i)
-        if n > 1:
-            factors.append(n)
-        return factors
+        # connect to queue
+        queue_client_tasks = QueueClient.from_connection_string(self._connect_str, q_name_tasks,
+                                message_encode_policy = BinaryBase64EncodePolicy(),
+                                message_decode_policy = BinaryBase64DecodePolicy())
+        queue_client_results = QueueClient.from_connection_string(self._connect_str, q_name_results,
+                                message_encode_policy = BinaryBase64EncodePolicy(),
+                                message_decode_policy = BinaryBase64DecodePolicy())
+        # queue_client_results.create_queue()
 
+        print('> Start Acquisition Control Worker <')
+
+        while(True):
+            messages = queue_client_tasks.receive_messages()
+
+            for message in messages:
+                print(message)
+                print("Dequeueing message: " + message.content.decode('UTF-8'))
+
+                if self.parseCommand(message.content.decode('UTF-8')):
+                    print('Commmand found.')
+                    queue_client_tasks.delete_message(message.id, message.pop_receipt)
+                else:
+                    print("Command not found.")
+
+                print("Adding message: " + message.content.decode('UTF-8'))
+                queue_client_results.send_message(message.content)
+
+            time.sleep(1)
+        print('> Stop Acquisition Control Worker <')
+
+    def parseCommand(self, command: str) -> bool:
+        for acquisitionCommand in AcquisitionCommands:
+            if acquisitionCommand.name == command:
+                self.signalCommand.emit(acquisitionCommand)
+                return True
+        return False
 
 #DEBUG
 class Window(QWidget):
@@ -114,6 +223,19 @@ class Window(QWidget):
     def updateStatus(self, status):
         self.label_status.setText(status)
 
+    @Slot(str)
+    def startMeasurement(self):
+        self.label_status.setText('Start Measurement')
+
+    @Slot(str)
+    def stopMeasurement(self):
+        self.label_status.setText('Stop Measurement')
+
+    @Slot(str)
+    def pauseMeasurement(self):
+        self.label_status.setText('Pause Measurement')
+
+
 if __name__=='__main__':
     app = QApplication(sys.argv)
     control = AcquisitionControl(app)
@@ -122,6 +244,9 @@ if __name__=='__main__':
     gui = Window()
     gui.button_cancel.clicked.connect(control.forceWorkerReset)
     control.signalStatus.connect(gui.updateStatus)
+    control.signalStartMeasurement.connect(gui.startMeasurement)
+    control.signalStopMeasurement.connect(gui.stopMeasurement)
+    control.signalPauseMeasurement.connect(gui.pauseMeasurement)
     gui.button_start.clicked.connect(control.signalStart)
     gui.show()
 
